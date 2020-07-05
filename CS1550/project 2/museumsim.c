66c12b1514878feb872cf458a8fcbe79893e4c18
adamkkarl <adamkkarl@gmail.com>
2020-07-05 12:18:44 -0400

    project 2 initialized

---
+++ CS1550/project 2/museumsim.c
@@ -0,0 +1,20 @@
+//a: visitor process
+
+//b: tour guide process
+
+//c1: visitorArrives()
+
+//c2: tourMuseum()
+
+//c3: visitorLeaves()
+
+//c4: tourguideArrives()
+
+//c5: openMuseum()
+
+//c6: tourguideLeaves()
+
+//d: process simulating tour guides' arrival
+
+//e: process simulating visitor's arrival
+
