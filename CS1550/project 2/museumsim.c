3673926bf7c5d145a3c9170850d9b68228009e4d
adamkkarl <adamkkarl@gmail.com>
2020-07-05 20:05:37 -0400

    initial solution using condition variables

--- CS1550/project 2/museumsim.c
+++ CS1550/project 2/museumsim.c
@@ -1,35 +1,95 @@
+#include <sys/mman.h>
+#include <condvar.c>
+#include <condvar.h>
+
+//integers shared across processes
+int *visitorsInMuseum;
+int *guidesInMuseum;
+
+//locks shared across processes
+struct cs1550_lock *lock;
+
+//CVs shared
+struct cs1550_condition *visitorGO, *guideGO, *guideEXIT;
+
+  
 //c1: visitorArrives()
+//arrive and wait (if necessary) until the number of visitors inside is < 10x the number of guides inside
 void visitorArrives() {
-  ;
+  cs1550_acquire(lock);
+  if(*visitorsInMuseum < 10 * *guidesInMuseum) {
+    //space is available inside, go right in
+    cs1550_release(lock);
+    return;
+  }
+
+  while(*visitorsInMuseum >= 10 * *guidesInMuseum) {
+    if(guidesInMuseum < 2) {
+      cs1550_signal(guideGO);
+    }
+    cs1550_wait(visitorGO);
+  }
+
+  //free to go inside
+  *visitorsInMuseum += 1;
+  cs1550_signal(visitorGO); //in case multiple visitors were waiting on guide
+  cs1550_release(lock);
+  return;
 }
 
 //c2: tourMuseum()
-//1 tour guide and at most 10 visitors
+//just sleep
 void tourMuseum() {
-	sleep(2);
+  sleep(2);
+  return;
 }
 
 //c3: visitorLeaves()
+//decrement the number of visitors inside
 void visitorLeaves() {
-  ;
+  cs1550_acquire(lock);
+  *visitorsInMuseum--; //decrement num visitors inside
+  cs1550_signal(visitorGO);
+  if(*visitorsInMuseum == 0) { //signal guide(s) to leave if no visitors left
+    cs1550_signal(guideEXIT);
+  }
+  cs1550_release(lock);
+  return;
 }
 
 //c4: tourguideArrives()
-//at most 2 tour guides in museum at a time
+// arrive and wait(if necessary) until
+// < 2 guides inside && visitor(s) waiting outside
 void tourguideArrives() {
-  ;
+  cs1550_acquire(lock);
+  cs1550_signal(visitorGO);
+  do {
+    //should always go to sleep until signalled by visitor waiting outside
+    cs1550_wait(guideGO);
+  } while(guidesInside >= 2);
+  *guidesInMuseum++;
+  cs1550_signal(visitorGO);
+  cs1550_release(lock);
+  return;
 }
 
 //c5: openMuseum()
-//tour guide cannot open museum without a visitor
+//simply increment the number of guides in the museum
 void openMuseum() {
-  ;
+  cs1550_acquire(lock);
+  *guidesInMuseum++;
+  cs1550_release(lock);
 }
 
 //c6: tourguideLeaves()
-//leaves when no more visitors are in the museum
+//wait until no more visitors are in the museum, then leave
 void tourguideLeaves() {
-  ;
+  cs1550_acquire(lock);
+  do {
+    cs1550_wait(guideEXIT);
+  } while(*visitorsInMuseum > 0);
+  *guidesInMuseum--;
+  cs1550_release(lock);
 }
 
 
@@ -42,6 +102,7 @@ int main(int argc, const char* argv[]) {
   int pg = 0;
   int dg = 1;
   int sg = 0;
+
 
   //read inputs
 	int i = 1;
@@ -66,8 +127,31 @@ int main(int argc, const char* argv[]) {
 		i += 2;
 	}
 
+  //ids for each visitor and guide (don't need to be locked)
   int visitorNumber = 0;
   int guideNumber = 0;
+
+  //initialize shared ints
+  void *ptr = mmap(NULL, 2*sizeof(int), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, 0,0);
+  visitorsInMuseum = (int*) ptr;
+  guidesInMuseum = visitorsInMuseum + 1;
+  *visitorsInMuseum = *guidesInMuseum = 0;
+  
+
+  //initialize the locks for the 2 shared ints
+  void *ptr = mmap(NULL, sizeof(struct cs1550_lock), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, 0,0);
+  lock = (struct cs1550_lock *) ptr;
+  cs1550_init_lock(lock, "lock");
+
+  //initialize 3 shared condition variables
+  void *ptr = mmap(NULL, 3*sizeof(struct cs1550_condition), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, 0,0);
+  visitorGO = (struct cs1550_condition *) ptr;
+  guideGO = visitorGO + 1;
+  guideEXIT = visitorGO + 2;
+  cs1550_init_condition(visitorGO, lock, "visitorGO");
+  cs1550_init_condition(guideGO, lock, "guideGO");
+  cs1550_init_condition(guideEXIT, lock, "guideEXIT");
+
 
 
   //initialize shared struct timeval startTime just before forking
@@ -99,10 +183,11 @@ int main(int argc, const char* argv[]) {
 
         tourMuseum();
         
+        visitorLeaves();
+        
         gettimeeofday(&currTime, NULL);
         fprintf(stderr, "Visitor %d leaves the museum at time %d.", id, currTime - startTime);
 
-        visitorLeaves();
 
         exit();
       } else {
@@ -116,7 +201,6 @@ int main(int argc, const char* argv[]) {
       }
     }
     wait();
-    exit();
   } else {
     //guide generator process
     
@@ -142,11 +226,11 @@ int main(int argc, const char* argv[]) {
 
         openMuseum();
 
+        tourguideLeaves();
+
         gettimeeofday(&currTime, NULL);
         fprintf(stderr, "Tour guide %d leaves the museum to time %d.", id, currTime - startTime);
         
-        tourguideLeaves();
-
         exit();
       } else {
         //continue with guide generation
@@ -159,6 +243,12 @@ int main(int argc, const char* argv[]) {
       }
     }
     wait();
-    exit();
   }
+  //close condition vars
+  cs1550_close_condition(visitorGO, lock, "visitorGO");
+  cs1550_close_condition(guideGO, lock, "guideGO");
+  cs1550_close_condition(guideEXIT, lock, "guideEXIT");
+
+  //close lock
+  cs1550_close_lock(lock);
 }
